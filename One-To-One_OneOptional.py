1cfb3f45e84dc3ce9461fdbc64b742e6e3a2c6c8
Lucas Tedim <56851341+Spectroll@users.noreply.github.com>
2023-09-07 13:55:25 -0300

    Update Attributes and AttAdding Method

--- One-To-One_OneOptional.py
+++ One-To-One_OneOptional.py
@@ -32,7 +32,7 @@ person = VGroup(person_title, person_shape, person_att, person_att_code, person_
 passp_title = Text("Passport", color = BLACK, font_size= 24)
 passp_shape = SurroundingRectangle(passp_title, color = BLACK, buff = 0.4)
 passp_att = Circle(radius= 0.1, color= BLACK, fill_opacity= 1).next_to(passp_shape.get_bottom(), DL*1.5)
-passp_att_code = Tex("Code", color= BLACK, font_size= 20).next_to(passp_att, DOWN, buff= 0.05)
+passp_att_code = Tex("ID", color= BLACK, font_size= 20).next_to(passp_att, DOWN, buff= 0.05)
 passp_att_con = Line(start= [-0.475, -0.55, 0], end= passp_att.get_top(), color= BLACK)
 
 passp = VGroup(passp_title, passp_shape, passp_att, passp_att_code, passp_att_con)
@@ -136,17 +136,17 @@ for item in range(3):
 manage_merge.scale(0.4)
 #---------------------------------------------------------------------------------------------------------------------------#
 
-#Modelo logico de empregado no metodo Adicao de Atributos
+#Modelo logico de pessoa no metodo Adicao de Atributos
 #---------------------------------------------------------------------------------------------------------------------------#
 person_att_adding = Table(
-    [["Veronica","14","Marketing"],
-    ["Roger","NULL","NULL"],
-    ["Elisa","11","Finance",],
-    ["Billy","13","Comercial"],
-    ["Adam","NULL","NULL"],
-    ["Alex","12","Administrative"]],
+    [["Veronica","NULL"],
+    ["Roger","02"],
+    ["Elisa","03"],
+    ["Billy","NULL"],
+    ["Adam","05"],
+    ["Alex","06"]],
     row_labels=[Text("01"), Text("02"), Text("03"), Text("04"), Text("05"), Text("06")],
-    col_labels=[Text("Name"), Text("Manages_Dept_Code"), Text("Manages_Dept_Name")],
+    col_labels=[Text("Name"), Text("Passp_ID")],
     top_left_entry=Text("ID"),
     include_outer_lines=True,
     line_config= {"color": BLACK},
@@ -172,8 +172,8 @@ fk = VGroup(line, arco, arco2, line2, line3)
 class Introduction(Scene):
     def construct(self):
         #Parte 1: Introducao de 1:1 (um lado opcional) e modelo exemplo
-        intro = Tex("We create one-to-one optional relationships when two entities may or may not have a relationship between them.", color= BLACK, font_size= 32).next_to(relation, UP, buff=3.5)
-        intro_2 = Tex("In the following case, only one side of the relation will be optional, while the other is mandatory", color= BLACK, font_size= 32).next_to(intro, DOWN, buff= 0.25)        
+        intro = Tex("A one-to-one optional relationship establishes that one instance from each participant entity may or may not have a connection between them.", color= BLACK, font_size= 32).next_to(relation, UP, buff=3.5)
+        intro_2 = Tex("In other words, only one side of the relationship will be optional in this relationship type, while the other is mandatory.", color= BLACK, font_size= 32).next_to(intro, DOWN, buff= 0.25)        
         intro_3 = Tex("Take this Entity-Relationship Diagram as example:", color= BLACK, font_size= 32).next_to(intro_2, DOWN, buff= 0.25)
         self.play(Write(intro), run_time= 3)
         self.wait(4)
@@ -207,7 +207,7 @@ class Introduction(Scene):
         self.play(relation.animate.scale(0.75))
         self.wait(2)
 
-        #Parte 3: Apresentacao dos metodos e inicio da fusao de entidade
+        #Parte 3: Apresentacao dos metodos
         to_elim = VGroup(intro, intro_2, intro_4)
         intro = Tex("There are 3 ways to deal with One-To-One relationships:", color= BLACK, font_size= 32).next_to(relation, UP, buff=3.5)
         intro_2 = Tex("Method 1: Entity Fusion", color= BLACK, font_size= 36).next_to(intro, DOWN, buff= 0.5)
@@ -231,7 +231,7 @@ class Entity_Fusion(Scene):
         #Parte 1: Introducao do metodo
         relation.move_to([0, 0.75, 0])
         title = Tex("Method 1: Entity Fusion", color= BLACK, font_size= 36).move_to([-5, 3.75, 0])
-        intro = Tex("For this method, we are going to merge both entities and the relationship into a single table:", color= BLACK, font_size= 32).next_to(relation, UP, buff= 1)
+        intro = Tex("This method implies merging both entities and the relationship into a single relation.", color= BLACK, font_size= 32).next_to(relation, UP, buff= 1)
 
         self.play(Write(title))
         self.wait()
@@ -269,11 +269,11 @@ class Entity_Fusion(Scene):
 
         #Parte 4: Conclusao
         pros = Tex("Pros:", color= PURE_GREEN, font_size= 40).move_to([-3, 3, 0])
-        pro_1 = Tex("-Eliminates the need for joints", color= BLACK, font_size= 32).next_to(pros, DOWN, buff= 0.25)
+        pro_1 = Tex("-Eliminates the need for relations joints", color= BLACK, font_size= 32).next_to(pros, DOWN, buff= 0.25)
         pro_2 = Tex("-Simple implementation", color= BLACK, font_size= 32).next_to(pro_1, DOWN, buff= 0.25)
         cons = Tex("Cons:", color= PURE_RED, font_size= 40).move_to([3, 3, 0])
-        con_1 = Tex("-Hinders future expansions", color= BLACK, font_size= 32).next_to(cons, DOWN, buff= 0.25)
-        con_2 = Tex(r"-Optional attributes may only apply \\to some records, resulting in lots of NULL values", color= BLACK, font_size= 32).next_to(con_1, DOWN, buff= 0.25)
+        con_1 = Tex(r"-Inhibits model evolution to attend \\ to new business needs", color= BLACK, font_size= 32).next_to(cons, DOWN, buff= 0.25)
+        con_2 = Tex(r"-Optional attributes may only apply \\ to some instances, resulting in many NULL values.", color= BLACK, font_size= 32).next_to(con_1, DOWN, buff= 0.25)
 
         self.play(FadeOut(intro))
         self.play(Write(pros))
@@ -289,7 +289,7 @@ class Attribute_Adding(Scene):
         #Parte 1: Introducao ao metodo
         relation.move_to([0, 0.75, 0])
         title = Tex("Method 2: Attribute adding", color= BLACK, font_size= 36).move_to([-4.8, 3.75, 0])
-        desc = Tex("For this method, add the optional entity's attributes to the mandatory entity's table.", color= BLACK, font_size= 32).next_to(relation, UP, buff=1)
+        desc = Tex("This method requires adding the attribute key from the mandatory relation to the optional relation.", color= BLACK, font_size= 32).next_to(relation, UP, buff=1)
 
         self.play(Write(title))
         self.wait()
@@ -301,15 +301,17 @@ class Attribute_Adding(Scene):
         self.play(FadeOut(desc))
         self.play(relation.animate.shift(UP * 2))
 
-        path = ArcBetweenPoints(start= person.get_center(), end= [-2, -2.5, 0], angle= 0.5)
+        path = ArcBetweenPoints(start= person.get_center(), end= [-2, -1.5, 0], angle= 0.5)
         path2 = ArcBetweenPoints(start= passp.get_center(), end= [2, -1.5, 0], angle= -0.5)
         person_copy = person.copy()
         passp_copy = passp.copy()
         
+        
         self.wait()
         self.play(MoveAlongPath(person_copy, path))
+        person_att_adding.move_to(person_copy.get_center()).shift(DOWN*0.5)
         self.wait()
-        self.play(FadeTransform(person_copy, logic_person))
+        self.play(FadeTransform(person_copy, person_att_adding))
         self.wait(2)
         self.play(MoveAlongPath(passp_copy, path2))
         logic_passp.move_to(passp_copy.get_center())
@@ -318,48 +320,34 @@ class Attribute_Adding(Scene):
         self.wait(2)
 
         # #Parte 3: Destacar atributos do relacionamento e adicionar ao modelo logico de person
-        desc2 = Tex("In this particular case, the ID in Person and Passport are the same attributes, so nothing will be added!", color= BLACK, font_size= 32).next_to(relation, DOWN, SMALL_BUFF)
-        
-        self.play(Write(desc2, run_time= 3))
-        self.wait(3)
-        # passp_code = VGroup(passp_att, passp_att_con, passp_att_code)
-        # path = ArcBetweenPoints(start= passp_code.get_center(), end= [-0.5, 0.5, 0], angle= -1.7)
+        passp_code = VGroup(passp_att, passp_att_con, passp_att_code)
+        path = ArcBetweenPoints(start= passp_code.get_center(), end= [-1.1, 0, 0], angle= -1.7)
 
-        # column_copy = person_att_adding.get_columns()[2].copy()
-        # column_copy.set_color(BLACK)
-        # column_copy2 = person_att_adding.get_columns()[3].copy()
-        # column_copy2.set_color(BLACK)
-        # passp_code_copy = passp_code.copy()
-        # passp_name_copy = passp_name.copy()
+        column_copy = person_att_adding.get_columns()[2].copy()
+        column_copy.set_color(BLACK)
+        passp_code_copy = passp_code.copy()
 
-        # self.play(MoveAlongPath(passp_code_copy, path))
-        # self.play(Circumscribe(passp_code_copy, Circle, fade_out= True, color= PURE_RED))
-        # self.wait()
-        # self.play(ReplacementTransform(passp_code_copy, column_copy))
-        # person_att_adding.add_highlighted_cell((1, 3), color= ORANGE)
-        # self.wait(2)
-
-        # self.play(MoveAlongPath(passp_name_copy, path2))
-        # self.play(Circumscribe(passp_name_copy, Circle, fade_out= True, color= PURE_RED))
-        # self.wait()
-        # self.play(ReplacementTransform(passp_name_copy, column_copy2))
-        # person_att_adding.add_highlighted_cell((1, 4), color= ORANGE)
-        # self.wait(2)
+        self.play(MoveAlongPath(passp_code_copy, path))
+        self.play(Circumscribe(passp_code_copy, Circle, fade_out= True, color= PURE_RED))
+        self.wait()
+        self.play(ReplacementTransform(passp_code_copy, column_copy))
+        person_att_adding.add_highlighted_cell((1, 3), color= ORANGE)
+        self.wait(2)
 
         #Parte 4: Representar o relacionamento pela chave estrangeira
-        exp = Tex("The relationship will be represented as a Foreign Key between Employee's and Department's table:", color= BLACK, font_size= 32).next_to(desc2, DOWN)
+        exp = Tex("The relationship will be represented as a Foreign Key between Employee's and Department's relation:", color= BLACK, font_size= 32).next_to(relation, DOWN)
         manage_copy = manage.copy()
         manage_copy.generate_target()
-        manage_copy.target.next_to(manage, DOWN, 2)#.scale(0.8)
+        manage_copy.target.next_to(manage, DOWN, 1.75)
         
-        line = Line(start= [-4.1, 0, 0], end= [0.75, 0, 0]).set_color(PURE_RED)
+        line = Line(start= [-2, 0, 0], end= [0.75, 0, 0]).set_color(PURE_RED)
         line2= Arrow(start= [0.85, -0.1, 0], end= [0.85, -0.47, 0], max_stroke_width_to_length_ratio= 10, max_tip_length_to_length_ratio= 0.5).set_color(PURE_RED)
-        line3= Arrow(start= [-4.2, -0.1, 0], end= [-4.2, -0.47, 0], max_stroke_width_to_length_ratio= 10, max_tip_length_to_length_ratio= 0.5).set_color(PURE_RED)
+        line3= Arrow(start= [-2.1, -0.1, 0], end= [-2.1, -0.47, 0], max_stroke_width_to_length_ratio= 10, max_tip_length_to_length_ratio= 0.5).set_color(PURE_RED)
         arco = ArcBetweenPoints(start= line.get_end(), end= line2.get_start(), angle = - PI/2).set_color(PURE_RED)
         arco2 = ArcBetweenPoints(start= line.get_start(), end= line3.get_start()).set_color(PURE_RED)
         fk = VGroup(line, arco, arco2, line2, line3)
         table_group = VGroup(logic_passp, logic_person)
-        fk.move_to(table_group.get_top()).shift(UP*0.25)
+        fk.move_to(table_group.get_top()).shift(UP*0.25).shift(RIGHT*1.1)
         key = Tex("FK", color= PURE_RED, font_size= 38).next_to(fk, UP, SMALL_BUFF)
 
         self.play(Write(exp))
@@ -372,17 +360,16 @@ class Attribute_Adding(Scene):
         self.wait()
 
         #Parte 5: Conclusao
-        final = VGroup(fk, key, logic_person, logic_passp)
+        final = VGroup(fk, key, person_att_adding, logic_passp, column_copy)
         pros = Tex("Pros:", color= PURE_GREEN, font_size= 40).move_to([-3, 3, 0])
         pro_1 = Tex("-Easy implementation", color= BLACK, font_size= 32).next_to(pros, DOWN, buff= 0.25)
         pro_2 = Tex("-Model is extendable", color= BLACK, font_size= 32).next_to(pro_1, DOWN, buff= 0.25)
-        pro_3 = Tex("-No null-values tuples", color= BLACK, font_size= 32).next_to(pro_2, DOWN, buff= 0.25)
         cons = Tex("Cons:", color= PURE_RED, font_size= 40).move_to([3, 3, 0])
         con_1 = Tex("-Joints necessary for searches.", color= BLACK, font_size= 40).next_to(cons, DOWN, buff= 0.25)
         arc = ArcBetweenPoints(start= relation.get_center(), end= [-4, -2, 0])
         anim = MoveAlongPath(relation, arc)
 
-        self.play(FadeOut(exp, desc2))
+        self.play(FadeOut(exp))
         self.play(final.animate.scale(0.75))
         self.play(final.animate.shift(RIGHT*3.5), anim, run_time= 3)
         self.wait()
@@ -390,7 +377,6 @@ class Attribute_Adding(Scene):
         self.play(Write(cons))
         self.play(Write(pro_1))
         self.play(Write(pro_2))
-        self.play(Write(pro_3))
         self.play(Write(con_1))
         self.wait(2)
 
@@ -399,7 +385,7 @@ class New_Relation(Scene):
         #Parte 1: apresentacao do metodo de nova relacao
         relation.move_to([0, 1, 0])
         title = Tex("Method 3: New Relation", color= BLACK, font_size= 36).move_to([-4.8, 3.7, 0])
-        desc = Tex("For this method, the entities will have their own separated tables.", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
+        desc = Tex("For this method, the entities will have their separate relation.", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
         logic_person.move_to([[-3.5, -2.45, 0]])
         logic_passp.move_to([[4, -1.9, 0]])
         path = ArcBetweenPoints(start= person.get_center(), end= logic_person.get_center(), angle= 1.7)
@@ -423,14 +409,14 @@ class New_Relation(Scene):
 
         #Parte 2: Transformacao da table de relacao
         to_elim = VGroup(desc)
-        desc = Tex("The relationship will also have its own table:", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
+        desc = Tex("Moreover, the relationship will also have its own relation.", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
         logic_manage.shift(UP*0.1)
         path = Line(manage.get_center(), logic_manage.get_center())
         manage_copy = manage.copy()
 
         self.play(FadeOut(to_elim))
         self.wait(2)
-        self.play(Write(desc))
+        self.play(Write(desc))  
         self.wait(2)
         self.play(MoveAlongPath(manage_copy, path))
         self.wait(2)
@@ -439,7 +425,7 @@ class New_Relation(Scene):
 
         #Parte 3: Destaca as chaves primárias
         to_elim = VGroup(desc)
-        desc = Tex("Note that the relationship's table is comprised by the primary keys of the entities:", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
+        desc = Tex("Note that the relationship's relation is comprised by the primary keys of the entities:", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
         id = VGroup(person_att, person_att_code, person_att_con)
         code = VGroup(passp_att, passp_att_code, passp_att_con)
         box_pk = SurroundingRectangle(id, corner_radius= 0.25, buff= 0.2, color= PURE_RED)
@@ -458,7 +444,7 @@ class New_Relation(Scene):
 
         #Parte 4: Destaca as chaves estrangeiras
         to_elim = VGroup(desc, box_pk, box_pk2, arr_pk, arr_pk2)
-        desc = Tex("Next, the primary key of each entity will be a foreign key to the relationship's table.", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
+        desc = Tex("Next, the primary key of each entity will be a foreign key to the relationship's relation.", color= BLACK, font_size= 32).next_to(relation, UP, buff=0.5)
         table_group = VGroup(logic_person, logic_manage)
         table_group2 = VGroup(logic_passp, logic_manage)
         line = Line(start= [-4.1, 0, 0], end= [-1, 0, 0]).set_color(PURE_RED)
@@ -489,12 +475,10 @@ class New_Relation(Scene):
         #Parte 5: Conclusao
         final = VGroup(fk, fk2, key, key2, logic_person, logic_passp, logic_manage)
         pros = Tex("Pros:", color= PURE_GREEN, font_size= 40).move_to([-3, 3, 0])
-        pro_1 = Tex("-Nothing :(", color= BLACK, font_size= 32).next_to(pros, DOWN, buff= 0.25)
+        pro_1 = Tex("-Model is VERY extendable", color= BLACK, font_size= 32).next_to(pros, DOWN, buff= 0.25)
         cons = Tex("Cons:", color= PURE_RED, font_size= 40).move_to([3, 3, 0])
-        con_1 = Tex("-More complicated searches.", color= BLACK, font_size= 40).next_to(cons, DOWN, buff= 0.25)
+        con_1 = Tex("-More complex relations' association.", color= BLACK, font_size= 40).next_to(cons, DOWN, buff= 0.25)
         con_2 = Tex("-Complex implementation", color= BLACK, font_size= 40).next_to(con_1, DOWN, buff= 0.25)
-        con_3 = Tex("-Redundant data", color= BLACK, font_size= 40).next_to(con_2, DOWN, buff= 0.25)
-        con_4 = Tex("-Higher number of tables", color= BLACK, font_size= 40).next_to(con_3, DOWN, buff= 0.25)
         arc = ArcBetweenPoints(start= relation.get_center(), end= [-4, -2, 0])
         anim = MoveAlongPath(relation, arc)
 
@@ -507,6 +491,4 @@ class New_Relation(Scene):
         self.play(Write(pro_1))
         self.play(Write(con_1))
         self.play(Write(con_2))
-        self.play(Write(con_3))
-        self.play(Write(con_4))
         self.wait(2)
